9a51dbf8c31f07d05975be81439191f1e8d315c0
isaelsilva1039@gmail.com <71187451+isaelsilva1039@users.noreply.github.com>
2021-01-29 17:18:01 -0300

    msgLogdos

--- model/conexao.php
+++ model/conexao.php
@@ -1,5 +1,5 @@
-  
 <?php
+
 try {
     $conn = new  PDO("mysql:dbname=de_volta;host=localhost", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -7,37 +7,37 @@ try {
     echo 'ERROR: ' . $e->getMessage();
 }
 
-// class Conexao
-// {
+class Conexao
+{
 
-//     public static $instace;
-//     public static function get_instace()
-//     {
+    public static $instace;
+    public static function get_instace()
+    {
 
-//         if (!isset(self::$instace)) {
-//             self::$instace = new PDO("mysql:dbname=de_volta;host=localhost", "root", "");
-//         }
-//         return self::$instace;
-//     }
+        if (!isset(self::$instace)) {
+            self::$instace = new PDO("mysql:dbname=de_volta;host=localhost", "root", "");
+        }
+        return self::$instace;
+    }
+}
+
+
+// try {
+//     $conn = new  PDO("mysql:dbname=u119780054_volta_php;host=localhost","u119780054_root","Isael99322065");
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     echo 'ERROR: ' . $e->getMessage();
 // }
 
 
-try {
-    $conn = new  PDO("mysql:dbname=u119780054_volta_php;host=localhost","u119780054_root","Isael99322065");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
-
-
-class Conexao{
+// class Conexao{
     
-    public static $instace ;
-    public static function get_instace(){
+//     public static $instace ;
+//     public static function get_instace(){
 
-        if(!isset(self::$instace)){
-            self::$instace = new PDO("mysql:dbname=u119780054_volta_php;host=localhost","u119780054_root","Isael99322065");
-    }
-        return self::$instace;        
-    }
-}
+//         if(!isset(self::$instace)){
+//             self::$instace = new PDO("mysql:dbname=u119780054_volta_php;host=localhost","u119780054_root","Isael99322065");
+//     }
+//         return self::$instace;        
+//     }
+// }
