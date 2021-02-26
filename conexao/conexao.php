c2e211b1137ca53f18b1b1d012c550e79c213820
isaelsilva1039 <isaelsilva1039@gmail.com>
2021-02-26 16:12:32 -0300

    conexão

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,11 +1,14 @@
-  
+
 <?php
+/*
 try {
     $conn = new  PDO("mysql:dbname=de_volta;host=localhost", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
+
+namespace\Conexao;
 
 class Conexao
 {
@@ -20,25 +23,26 @@ class Conexao
         return self::$instace;
     }
 }
+/*/
 
 
-// try {
-//     $conn = new  PDO("mysql:dbname=u119780054_volta_php;host=localhost","u119780054_root","Isael99322065");
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     echo 'ERROR: ' . $e->getMessage();
-// }
+try {
+    $conn = new  PDO("mysql:dbname=u119780054_volta_php;host=localhost","u119780054_root","Isael99322065");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
 
 
-// class Conexao{
-    
-//     public static $instace ;
-//     public static function get_instace(){
+class Conexao{
 
-//         if(!isset(self::$instace)){
-//             self::$instace = new PDO("mysql:dbname=u119780054_volta_php;host=localhost","u119780054_root","Isael99322065");
-//     }
-//         return self::$instace;        
-//     }
-// }
+    public static $instace ;
+    public static function get_instace(){
+
+        if(!isset(self::$instace)){
+            self::$instace = new PDO("mysql:dbname=u119780054_volta_php;host=localhost","u119780054_root","Isael99322065");
+        }
+        return self::$instace;
+    }
+}
   
