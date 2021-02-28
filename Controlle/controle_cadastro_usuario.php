d44017bdcf516d9bf6a882e2e9ec548cc34e3902
isaelsilva1039 <isaelsilva1039@gmail.com>
2021-02-28 03:01:05 -0300

    perfil

--- Controlle/controle_cadastro_usuario.php
+++ Controlle/controle_cadastro_usuario.php
@@ -1,5 +1,18 @@
 <?php
 include '../model/Class.configue.php';
+/*
+$foto = $_FILES['foto'];
+var_dump($foto['name']);
+var_dump($_FILES);
+*/
+//arquivo compelo array
+$foto = $_FILES['foto'];
+//nomr do arquivo
+$fotoNome = $_FILES['foto']['name'];
+//nome temporario
+$tmp_name = $_FILES['foto']['tmp_name'];
+//movendo o arquivo
+$up = move_uploaded_file($_FILES['foto']['tmp_name'],'../nice-html/img/'.$fotoNome);
 
 @$nome = $_POST['nome'];
 @$email = $_POST['email'];
@@ -7,9 +20,12 @@ include '../model/Class.configue.php';
 @$nivel = $_POST['nivel'];
 
 $ObjCadastros = new Validacao();
+$ObjCadastros->foto = $fotoNome;
 $ObjCadastros->nome = $nome;
 $ObjCadastros->email = $email;
 $ObjCadastros->senha = $senha;
 $ObjCadastros->nivel = $nivel;
 $ObjCadastros->cadastrasUsuariosComNivelDeAcessos();
 
+//não conseguir finalizar o update da imagem,
+// deu probelma pra envia do dormulario pra pag controle
--- Controlle/controle_update_perfil_imagem.php
+++ Controlle/controle_update_perfil_imagem.php
@@ -1,4 +1,5 @@
 <?php
+
 include('../model/Class.configue.php');
 
 $foto = $_FILES['foto'];
@@ -15,15 +16,8 @@ $up = move_uploaded_file($_FILES['foto']['tmp_name'], '../nice-html/img/' . $fot
 
 
 @$id = $_POST['id'];
-@$nome = $_POST['nome'];
-@$email = $_POST['email'];
-@$senha = $_POST['senha'];
-
 
 $valida = new Validacao();
 $valida->idUsuario = $id;
-var_dump($valida->foto = $fotoNome);
-$valida->nomeUsuario = $nome;
-$valida->emailUsuario = $email;
-$valida->senhaUsuario = $senha;
-$valida->uoDatePerfilUsuarioLogado();
+$valida->foto = $fotoNome;
+$valida->updadePerfilDaimagemDoUsuarioLogado();
