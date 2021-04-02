bcd1477e7a6eac0cf19ff83e7806b950caaa89da
isaelsilva1039 <isaelsilva1039@gmail.com>
2021-04-02 17:40:10 -0300

    escalaInventariobug

--- inventario/Calendario.php
+++ inventario/Calendario.php
@@ -7,7 +7,7 @@ class Calendario extends Conexao
     public function ExibirCaledario()
     {
         $pdo = parent::get_instace();
-        $sql = "SELECT * FROM  tb_calendario order by inicio";
+        $sql = "SELECT * FROM  tb_calendario order by incio";
         $sql = $pdo->prepare($sql);
         $sql->execute();
         return $sql->fetchAll();
